2808f634f1bafd412d705a09c335690c8b2fd821
rakpinar <akpinarrumeysa1@gmail.com>
2023-02-19 17:45:07 +0300

    Ecole42 second project

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rakpinar <rakpinar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 17:14:31 by rakpinar          #+#    #+#             */
+/*   Updated: 2023/02/19 17:28:12 by rakpinar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_get_read(int fd, char *str);
+int		ft_strchr(char *str, int c);
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_line(char *str);
+char	*ft_get_new_str(char *str);
+
+#endif
